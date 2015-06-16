62d7334e7824c1fa8ed1318c016c047a6882cbff
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-06-15 17:41:53 -0700

    multiprocessed animated-gif-compare

--- bin/animated-gif-compare.py
+++ bin/animated-gif-compare.py
@@ -4,45 +4,56 @@
 """ creates animated GIFs of each taxon distribution from old and new distribution hdf5 files, and generates an
 index.html for comparison """
 
+from multiprocessing import Pool
 import os
 
 import h5py
-import matplotlib.pyplot as plt
-from matplotlib import animation
 
 
-def animate(nframe):
-    plt.cla()
-    plt.imshow(grids[nframe])
-    plt.title(nframe)
+def process_taxon(gif_name, grids):
+    import matplotlib.pyplot as plt
+    from matplotlib import animation
+
+    def animate(nframe):
+        plt.cla()
+        plt.imshow(grids[nframe])
+        plt.title(nframe)
+
+    fig = plt.figure(figsize=(10, 8))
+    anim = animation.FuncAnimation(fig, animate, frames=2)
+    anim.save(gif_name, writer='imagemagick', fps=2)
+    plt.close()
+    print("wrote {}".format(gif_name))
 
 archive = h5py.File('archive-species-distribution.hdf5', 'r')
 distribution = h5py.File('species-distribution.hdf5', 'r')
 
 taxa = distribution['taxa'].keys()
 
+pool = Pool(8)
+
 with open('gifs/index.html', 'w') as index:
 
     index.write('<html><body>')
     for taxon in taxa:
-        print(taxon)
         gif_name = 'gifs/' + str(taxon) + '.gif'
+        index.write('<img title="{}" style="float: left;" src="{}"><br />'.format(str(taxon), str(taxon) + '.gif'))
+
         if os.path.isfile(gif_name):
             print("{} exists, skipping".format(gif_name))
             continue
 
         try:
             grids = []
-            grids.append(archive['taxa/' + str(taxon)])
-            grids.append(distribution['taxa/' + str(taxon)])
+            grids.append(archive['taxa/' + str(taxon)][:])
+            grids.append(distribution['taxa/' + str(taxon)][:])
         except KeyError:
             print("{} doesn't exist in archive".format(taxon))
             continue
 
-        fig = plt.figure(figsize=(20, 16))
-        anim = animation.FuncAnimation(fig, animate, frames=2)
-        anim.save(gif_name, writer='imagemagick', fps=2)
-        plt.close()
-        index.write('<img title="{}" style="float: left;" src="{}"><br />'.format(str(taxon), str(taxon) + '.gif'))
+        pool.apply_async(process_taxon, [gif_name, grids])
 
     index.write('</body></html>')
+
+pool.close()
+pool.join()
