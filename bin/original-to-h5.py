ce953ce1f892ac3c6463496ef29df7f4159b0884
Joseph Sheedy <joseph.sheedy@gmail.com>
2015-07-06 14:36:40 -0700

    generalized to create hdf5 from both taxon_distribution and taxon_distribution_archive

--- bin/original-to-h5.py
+++ bin/original-to-h5.py
@@ -9,36 +9,39 @@ import numpy as np
 
 from species_distribution.models.db import engine
 
-fname = 'archive-species-distribution.hdf5'
 
-distribution_file = h5py.File(fname, 'w')
+def create_hdf5(fname, table_name):
+    distribution_file = h5py.File(fname, 'w')
 
-distribution_file.create_group('taxa')
+    distribution_file.create_group('taxa')
 
-distribution_file['latitude'] = np.arange(89.75, -90, -.5)
-distribution_file['longitude'] = np.arange(-179.75, 180, .5)
+    distribution_file['latitude'] = np.arange(89.75, -90, -.5)
+    distribution_file['longitude'] = np.arange(-179.75, 180, .5)
 
-with engine.connect() as connection:
-    # get raw connection
-    raw_conn = connection.connection.connection
-    cursor = raw_conn.cursor()
-    cursor.execute('SELECT DISTINCT taxonkey FROM taxon_distribution_archive ORDER BY taxonkey')
+    with engine.connect() as connection:
+        # get raw connection
+        raw_conn = connection.connection.connection
+        cursor = raw_conn.cursor()
+        cursor.execute('SELECT DISTINCT taxonkey FROM {} ORDER BY taxonkey'.format(table_name))
 
-    for taxonkey in cursor:
-        taxonkey = taxonkey[0]
-        print("key: {}".format(taxonkey))
+        for taxonkey in cursor:
+            taxonkey = taxonkey[0]
+            print("key: {}".format(taxonkey))
 
-        cursor2 = raw_conn.cursor()
-        cursor2.execute('select cellid-1, relativeabundance from taxon_distribution_archive where taxonkey=%s ORDER BY cellid', (taxonkey,))
-        # create a grid populated with null values, then define values where cellid exists
-        x, y = map(np.array, zip(*cursor2))
-        data = np.full(360 * 720, np.nan)
-        data[x] = y
-        grid = data.reshape((360, 720))
+            cursor2 = raw_conn.cursor()
+            cursor2.execute('select cellid-1, relativeabundance from {} where taxonkey=%s ORDER BY cellid'.format(table_name), (taxonkey,))
+            # create a grid populated with null values, then define values where cellid exists
+            x, y = map(np.array, zip(*cursor2))
+            data = np.full(360 * 720, np.nan)
+            data[x] = y
+            grid = data.reshape((360, 720))
 
-        dataset = distribution_file.create_dataset('taxa/' + str(taxonkey), data=grid)
+            dataset = distribution_file.create_dataset('taxa/' + str(taxonkey), data=grid)
 
-        dataset.dims[0].attach_scale(distribution_file['latitude'])
-        dataset.dims[1].attach_scale(distribution_file['longitude'])
+            dataset.dims[0].attach_scale(distribution_file['latitude'])
+            dataset.dims[1].attach_scale(distribution_file['longitude'])
 
-distribution_file.close()
+    distribution_file.close()
+
+create_hdf5('archive-species-distribution.hdf5', 'taxon_distribution_archive')
+create_hdf5('species-distribution.hdf5', 'taxon_distribution')
