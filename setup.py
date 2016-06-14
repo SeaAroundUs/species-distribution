75c9865030aaf82f1f766c2c4953445d6c7afb9b
Rebel Scum <linht@vulcan.com>
2016-06-14 13:01:58 -0700

    Updated to add the Pillow package as it is actually required when we enter the code path to generate png files

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='Species distribution for Sea Around Us Project',
     test_suite='unittest2.collector',
     packages=find_packages(),
-    install_requires=['Cython', 'six', 'unittest2', 'numpy', 'psycopg2', 'python-dateutil', 'SQLAlchemy', 'pyproj', 'matplotlib'],
+    install_requires=['Cython', 'six', 'unittest2', 'numpy', 'psycopg2', 'python-dateutil', 'SQLAlchemy', 'pyproj', 'matplotlib', 'pillow'],
     scripts=[
         'bin/h5-to-database',
         'bin/h5-to-png',
